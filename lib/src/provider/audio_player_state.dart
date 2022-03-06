3e3e1f944571ad20eb985322ac9cee8027d61f3f
akshay <akshayasok1998@gmail.com>
2022-03-06 11:23:57 +0530

    feat: add waveform

--- lib/src/provider/audio_player_state.dart
+++ lib/src/provider/audio_player_state.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:just_waveform/just_waveform.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 const url = 'https://samplelib.com/lib/preview/mp3/sample-15s.mp3';
 final audioPlayerState = ChangeNotifierProvider((ref) => AudioPlayerState());
@@ -12,7 +18,7 @@ class AudioPlayerState extends ChangeNotifier {
   bool isPlaying = false;
 
   Future<void> setUp() async {
-    await player.setUrl(url);
+    totalDuration = (await player.setUrl(url)) ?? Duration.zero;
     player.positionStream.listen((event) {
       currentPosition = player.position;
       notifyListeners();
@@ -29,5 +35,23 @@ class AudioPlayerState extends ChangeNotifier {
     isPlaying = false;
     player.stop();
     notifyListeners();
+  }
+}
+
+final waveForm = ChangeNotifierProvider((ref) => AudioWaveForm());
+
+class AudioWaveForm extends ChangeNotifier {
+  Waveform? waveform;
+
+  Future init() async {
+    final audioFile = File(p.join((await getTemporaryDirectory()).path, 'waveform.mp3'));
+    await audioFile.writeAsBytes((await rootBundle.load('assets/sample.mp3')).buffer.asUint8List());
+    final waveFile = File(p.join((await getTemporaryDirectory()).path, 'waveform.wave'));
+    await for (var i in JustWaveform.extract(audioInFile: audioFile, waveOutFile: waveFile)) {
+      print(i.progress);
+      if (i.progress == 1) {
+        waveform = i.waveform!;
+      }
+    }
   }
 }
--- lib/src/screen/home.dart
+++ lib/src/screen/home.dart
@@ -3,9 +3,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../provider/audio_player_state.dart';
 import '../theme/color.dart';
+import '../utils/duration.dart';
+import 'widget/audio_waveform_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(audioPlayerState).setUp();
+    ref.read(waveForm).init();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +27,10 @@ class HomeScreen extends StatelessWidget {
       body: Consumer(
         builder: (_, ref, __) {
           final data = ref.watch(audioPlayerState);
+          final waveform = ref.watch(waveForm);
           return Center(
             child: Container(
-              width: MediaQuery.of(context).size.width / 1.5,
+              width: MediaQuery.of(context).size.width / 1.2,
               decoration: BoxDecoration(
                 color: AppColor.black,
                 borderRadius: BorderRadius.circular(24),
@@ -23,30 +38,50 @@ class HomeScreen extends StatelessWidget {
               padding: const EdgeInsets.all(30),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const Text(
                     'What does sports mean to you?',
                     style: TextStyle(
                       color: AppColor.white,
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                  const SizedBox(height: 12),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      IconButton(
-                        icon: Icon(
+                      GestureDetector(
+                        child: Icon(
                           data.isPlaying ? Icons.pause : Icons.play_arrow,
                           color: AppColor.white,
                         ),
-                        onPressed: () {
+                        onTap: () {
                           data.isPlaying ? data.stop() : data.play();
                         },
                       ),
+                      if (waveform.waveform != null)
+                        Expanded(
+                          child: SizedBox(
+                            height: 50.0,
+                            child: AudioWaveformWidget(
+                              waveform: waveform.waveform!,
+                              duration: waveform.waveform!.duration,
+                              start: data.currentPosition,
+                              waveColor: AppColor.white,
+                              strokeWidth: 1,
+                              pixelsPerStep: 3,
+                              scale: 0.6,
+                            ),
+                          ),
+                        )
                     ],
                   ),
                   Align(
                     alignment: Alignment.bottomRight,
                     child: Text(
-                      data.currentPosition.inSeconds.toString(),
+                      getTimeFromDuration(data.totalDuration),
                       style: const TextStyle(
                         color: AppColor.white,
                       ),
